8825c1b7741affeb7bca0be5000bbf26f8525bba
Dave Sayer <hello@crafted.im>
2018-10-25 23:38:23 +0100

    better styling of login and sign up

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -28,32 +28,38 @@ class SignUp extends Component {
     if (auth.uid) return <Redirect to='/dashboard' />
 
     return (
-      <div className='container'>
-        <h2>Sign Up</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="firstName">First name</label>
-            <input type="text" className="form-control" id="firstName" aria-describedby="emailHelp" placeholder="Enter First Name" onChange={this.handleChange} />
+      <section className="d-flex align-items-center justify-content-center">
+        <div className='container '>
+          <div className="row">
+            <div className="col col-sm-8 col-lg-4 mx-auto">
+              <h2>Sign Up</h2>
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">First name</label>
+                  <input type="text" className="form-control" id="firstName" aria-describedby="emailHelp" placeholder="Enter First Name" onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastName">Last name</label>
+                  <input type="text" className="form-control" id="lastName" aria-describedby="emailHelp" placeholder="Enter First Name" onChange={this.handleChange} />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email address</label>
+                  <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleChange} />
+                  <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.handleChange} />
+                </div>
+                <button type="submit" className="btn btn-primary">Sign Up</button>
+                <div>
+                  { authError ? <p>{ authError }</p> : null }
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="lastName">Last name</label>
-            <input type="text" className="form-control" id="lastName" aria-describedby="emailHelp" placeholder="Enter First Name" onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email address</label>
-            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleChange} />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.handleChange} />
-          </div>
-          <button type="submit" className="btn btn-primary">Sign Up</button>
-          <div>
-            { authError ? <p>{ authError }</p> : null }
-          </div>
-        </form>
-      </div>
+        </div>
+      </section>
     )
   }
 }
