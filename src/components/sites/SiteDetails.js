d776583ca504792cef7e9fdf7bad753c8378f48c
Dave Sayer <hello@crafted.im>
2018-11-04 02:04:37 +0000

    Adding Moment for date formatting and also Fixes #17.

--- src/components/sites/SiteDetails.js
+++ src/components/sites/SiteDetails.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import Moment from 'react-moment'
 // import { Redirect } from 'react-router-dom'
 import SiteMap from './SiteMap'
 
@@ -11,46 +12,48 @@ const SiteDetails = (props) => {
 
     const { site } = props
 
-    // console.log('Site details props: ', props);
+    console.log('Site details props: ', props);
     // if(!auth.uid) return <Redirect to='/signin' />;
 
     if (site){
       return (
-        <section id="site-details" className="d-flex align-items-start justify-content-center free-height" onLoad={window.scrollTo(0, 0)}>
-          <div className='container-fluid'>
-            <div className="row">
-              <div className="col site-map fixed">
-                <SiteMap site={site} />
+        <div className="wrapper">
+          <section id="site-details" className="d-flex align-items-start justify-content-center free-height site-details" onLoad={window.scrollTo(0, 0)}>
+            <div className='container'>
+              <div className="row">
+                <div className="col mx-auto site-map">
+                  <SiteMap site={site} />
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col col-10 col-lg-8 mx-auto site-info">
-                  <h2 className=''>{site.siteName}</h2>
-                  <p className="lead">{site.summary}</p>
-                  <p className=''>{site.description}</p>
+              <div className="row">
+                <div className="col col-10 col-lg-8 mx-auto site-info">
+                    <h2 className=''>{site.siteName}</h2>
+                    <p className="lead">{site.summary}</p>
+                    <p className=''>{site.description}</p>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col col-10 col-lg-8 mx-auto site-data">
-                <h3>Site data</h3>
-                <div className="row">
-                  <div className="col col-6">
-                    <dl className="data-list d-flex flex-row flex-wrap">
-                      <dt>Geo Location</dt>
-                      <dd>Lat: {site.lat} / Lng: {site.lng}</dd>
-                      <dt>Location</dt>
-                      <dd>{site.location}</dd>
-                      <dt>Added by</dt>
-                      <dd>{site.userFirstName} {site.userLastName}</dd>
-                      <dt>Location</dt>
-                      <dd>{site.location}</dd>
-                    </dl>
+              <div className="row">
+                <div className="col col-10 col-lg-8 mx-auto site-data">
+                  <h3>Site data</h3>
+                  <div className="row">
+                    <div className="col col-6">
+                      <dl className="data-list d-flex flex-row flex-wrap">
+                        <dt>Geo Location</dt>
+                        <dd>Lat: {site.lat} / Lng: {site.lng}</dd>
+                        <dt>Location</dt>
+                        <dd>{site.location}</dd>
+                        <dt>Added by</dt>
+                        <dd>{site.userFirstName} {site.userLastName}</dd>
+                        <dt>Date added</dt>
+                        <dd><Moment date={site.createdAt} /></dd>
+                      </dl>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       )
     } else {
       return (
